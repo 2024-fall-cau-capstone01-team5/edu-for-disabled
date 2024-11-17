0040fbbbde29865d498279f31d254a2b785d51f2
kimseongsun <kimseongson@gmail.com>
2024-11-17 21:22:08 +0900

    디버깅 코드 제거

--- lib/providers/Scenario_c_provider.dart
+++ lib/providers/Scenario_c_provider.dart
@@ -19,23 +19,23 @@ class Sinario_c_provider extends Scenario_Manager {
 
   @override
   List<Widget> get leftScreen => [
-    // c_1_enterTheStore_left(),
-    // c_2_enterTheStore_left(),
+    c_1_enterTheStore_left(),
+    c_2_enterTheStore_left(),
     // c_3_display_left(),
-    // c_4_display_left(),
-    // c_5_display_left(),
-    // c_6_display_left(),
+    c_4_display_left(),
+    c_5_display_left(),
+    c_6_display_left(),
     c_7_congratuations_left(),
   ];
 
   @override
   List<Widget> get rightScreen => [
-    // c_1_enterTheStore_right(),
-    // c_2_enterTheStore_right(),
+    c_1_enterTheStore_right(),
+    c_2_enterTheStore_right(),
     // c_3_display_right(),
-    // c_4_display_right(),
-    // c_5_display_right(),
-    // c_6_display_right(),
+    c_4_display_right(),
+    c_5_display_right(),
+    c_6_display_right(),
     c_7_congratuations_right()
   ];
 
