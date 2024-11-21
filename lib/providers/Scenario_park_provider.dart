cd431e5b3817b274f252a77c319ae60d4d25617c
kimseongsun <kimseongson@gmail.com>
2024-11-22 04:59:10 +0900

    연결 작업 시작

--- lib/providers/Scenario_park_provider.dart
+++ lib/providers/Scenario_park_provider.dart
@@ -2,6 +2,21 @@ import 'package:flutter/material.dart';
 import 'Scenario_Manager.dart';
 import '../scenarios/StepData.dart';
 
+import '../scenarios/park/Scenario_park_1.dart';
+import '../scenarios/park/Scenario_park_2.dart';
+import '../scenarios/park/Scenario_park_3.dart';
+import '../scenarios/park/Scenario_park_4.dart';
+import '../scenarios/park/Scenario_park_5.dart';
+import '../scenarios/park/Scenario_park_6.dart';
+import '../scenarios/park/Scenario_park_7.dart';
+import '../scenarios/park/Scenario_park_8.dart';
+
+import '../scenarios/common_scenario/go_outside.dart';
+import '../scenarios/common_scenario/elevator_1.dart';
+import '../scenarios/common_scenario/elevator_2.dart';
+import '../scenarios/common_scenario/elevator_3.dart';
+
+
 class Scenario_park_provider extends Scenario_Manager {
 
   final String learningLogId;
@@ -13,16 +28,39 @@ class Scenario_park_provider extends Scenario_Manager {
   }
 
   @override
-  String get title => "편의점을 가보자!";
+  String get title => "공원을 가보자!";
 
-  //String 살거 물품;
 
   @override
   List<Widget> get leftScreen => [
+    Go_outside_left(),
+    Elevator_1_left(),
+    Elevator_2_left(),
+    Elevator_3_left(),
+    Scenario_park_1_left(),
+    Scenario_park_2_left(),
+    Scenario_park_3_left(),
+    Scenario_park_4_left(),
+    Scenario_park_5_left(),
+    Scenario_park_6_left(),
+    Scenario_park_7_left(),
+    Scenario_park_8_left(),
   ];
 
   @override
   List<Widget> get rightScreen => [
+    Go_outside_right(),
+    Elevator_1_right(step_data: step_data),
+    Elevator_2_right(),
+    Elevator_3_right(step_data: step_data),
+    Scenario_park_1_right(),
+    Scenario_park_2_right(),
+    Scenario_park_3_right(),
+    Scenario_park_4_right(),
+    Scenario_park_5_right(),
+    Scenario_park_6_right(),
+    Scenario_park_7_right(),
+    Scenario_park_8_right(),
   ];
 
 
